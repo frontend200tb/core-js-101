956c425c46609b29a8c4fb2c74ad6d22ccaf4e68
Frontend200tb <frontend200tb@mail.ru>
2023-09-04 00:18:01 +0300

    feat: 42.Transforms the numeric array to the according moving sum array

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -241,8 +241,16 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  let i = arr[0];
+  return arr.map((item, index) => {
+    if (index === 0) {
+      i = item;
+    } else {
+      i += item;
+    }
+    return i;
+  });
 }
 
 /**
