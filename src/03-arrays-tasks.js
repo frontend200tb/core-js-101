1ec8dd600e617c774e82fb409800d625e9148e87
Frontend200tb <frontend200tb@mail.ru>
2023-09-09 00:24:09 +0300

    feat: 56.Groups elements of the specified array by key

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -532,8 +532,11 @@ function distinct(arr) {
  *    "Poland" => ["Lodz"]
  *   }
  */
-function group(/* array, keySelector, valueSelector */) {
-  throw new Error('Not implemented');
+function group(array, keySelector, valueSelector) {
+  const map = new Map([...new Set(array.map(keySelector))].map((item) => [item, []]));
+  array.map((item) => map.set(keySelector(item),
+    [...map.get(keySelector(item)), valueSelector(item)]));
+  return map;
 }
 
 
