99dd73aedd9f3f1e6fa0dcdc7a18ebe1e7de806e
Frontend200tb <frontend200tb@mail.ru>
2023-09-09 20:38:02 +0300

    feat: 64.Returns true, if two specified axis-aligned rectangles overlap

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -144,8 +144,19 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+/* Сначала проверяем что верхняя сторона первого прямоугольника
+лежит между верхней и нижней стороной второго (и наоборот)
+потом что левая сторона первого прямоугольника
+лежит между левой и правой стороной второго и наоборот */
+function doRectanglesOverlap(rect1, rect2) {
+  let result = false;
+  if (((rect1.top >= rect2.top && rect1.top <= (rect2.top + rect2.height))
+    || (rect2.top >= rect1.top && rect2.top <= (rect1.top + rect1.height)))
+    && ((rect1.left >= rect2.left && rect1.left <= (rect2.left + rect2.width))
+    || (rect2.left >= rect1.left && rect2.left <= (rect1.left + rect1.width)))) {
+    result = true;
+  }
+  return result;
 }
 
 
