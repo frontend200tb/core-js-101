05e9b2d6db01791f8547f9e73c89a11774785168
Frontend200tb <frontend200tb@mail.ru>
2023-09-10 14:02:55 +0300

    feat: 71.Returns the digital root of integer

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -342,8 +342,12 @@ function isCreditCardNumber(ccn) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  let result = num;
+  while (result > 9) {
+    result = result.toString().split('').reduce((a, b) => +a + +b);
+  }
+  return result;
 }
 
 
